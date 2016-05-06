5dcae09e0915bbd258e56af969d72330cb0241a1
Sunday Adefila <adefilaedward@gmail.com>
2016-05-06 10:39:44 +0100

    pictures blob getting saved to local storage

--- dev_pic.js
+++ dev_pic.js
@@ -1,16 +1,53 @@
 var dev_pics_cols = document.getElementsByClassName('dev-pics-cols');
 var pic_dir = '/Users/Sunday/Pictures/tkt/'
 
+var errorHandler = function(){
+	console.log('error');
+};
 
-// From the File API documentations:
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
 
-/*
-window.requestFileSystem(window.TEMPORARY, 1024*1024, function(fs) {
-  fs.root.getDirectory('MyPictures', {create: true}, function(dirEntry) {
-    
-  }, errorHandler);
-}, errorHandler);
-*/
+  // Loop through the FileList and render image files as thumbnails.
+  for (var i = 0, f; f = files[i]; i++) {
+
+    // Only process image files.
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+
+    var reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+      return function(e) {
+      	// Save files to locasStorage
+
+      	localStorage.setItem('myPictures', e.target.result);
+
+      	var savedPics = localStorage.getItem('myPictures');
+
+      	if(savedPics){
+      		alert(savedPics);
+      	}
+
+
+
+        // Render thumbnail.
+        // var span = document.createElement('span');
+        // span.innerHTML = ['<img class="thumb" src="', e.target.result,
+        //                   '" title="', escape(theFile.name), '"/>'].join('');
+        // document.getElementById('list').insertBefore(span, null);
+      };
+    })(f);
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+}
+
+document.getElementById('myFiles').addEventListener('change', handleFileSelect, false);
+
 
 
 // THIS DID NOT WORK BECAUSE OF THE NOTORIOUS XSS SECURITY ISSUE:
